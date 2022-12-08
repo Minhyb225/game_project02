ef232c5457a8a8a7a51b6810f89774bbd30d1f11
AnnaHyttinen <anna.maija.hyttinen@gmail.com>
2022-12-08 11:00:14 +0200

    Here goes some sound and carbon.

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+# if it is anything like in the first version of the game:
+# result_2 = result[2]
+# ,but for now:
+result_2 = 1000
+def Carbon(co):
+    if co < 550:
+        co += (co + co * 0.1) * 10
+        return co
+    else:
+        co += (co + 50) * 10
+        return co
+co2_consumed = Carbon(result_2)
+print(co2_consumed)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import winsound
+def Sound(file):
+    if file == 1:
+        choice = 'sounds/Choise1.wav'
+        winsound.PlaySound(choice, winsound.SND_FILENAME)
+    elif file == 2:
+        bummer = 'sounds/Bummer.wav'
+        winsound.PlaySound(bummer, winsound.SND_FILENAME)
+    elif file == 3:
+        progress = 'sounds/Progress1.wav'
+        winsound.PlaySound(progress, winsound.SND_FILENAME)
+    elif file == 4:
+        loopy = 'sounds/Loopy tune.wav'
+        winsound.PlaySound(loopy, winsound.SND_FILENAME)
+Sound(1)
+Sound(2)
+Sound(3)
+Sound(4)
