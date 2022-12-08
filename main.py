f67671aaf55469fbc1db3212925b0cd48446299a
AnnaHyttinen <anna.maija.hyttinen@gmail.com>
2022-12-08 17:55:15 +0200

    Dealing with the database.

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-# if it is anything like in the first version of the game:
-# result_2 = result[2]
-# ,but for now:
-result_2 = 1000
+# to fetch the previous co2_consumed from sql:
+import mysql.connector
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='********',
+         autocommit=True
+         )
+# where do I get the screen name from?
+# The data can also be cleaned after each game, if needed.
+# For purposes of this example:
+screen_name = 'Ilkka'
+
+sql = "SELECT co2_consumed FROM game WHERE screen_name=' + screen_name + ';"
+cursor = connection.cursor()
+cursor.execute(sql)
+result_2 = cursor.fetchall()
+print(f"{result_2}.")
+
 def Carbon(km_latest):
     if km_latest < 550:
         co = km_latest
@@ -14,6 +31,13 @@ def Carbon(km_latest):
 # here we call this function:
 co2_consumed = Carbon(result_2)
 print(co2_consumed)
+
+
+# after calculating co2:
+sql = "UPDATE game SET co2_consumed =' + co2_consumed + ' WHERE screen_name =' + screen_name + ';"
+cursor = connection.cursor()
+cursor.execute(sql)
+
 
 
 import winsound
