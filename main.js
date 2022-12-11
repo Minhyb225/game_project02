6c30525293b27862a246a6d128ca7e3cb56c7083
nguyenminh <minh.nguyen20@metropolia.fi>
2022-12-11 18:43:25 +0200

    02

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
+function runMinigame() {
+//document.addEventListener('DOMContentLoaded', () => {
   //card options
   const cardArray = [
     {
@@ -62,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
   //create your board
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
-      const card = document.createElement('img')
+      let card = document.createElement('img')
       card.setAttribute('src', cardArray[i].img)
       card.setAttribute('id', i)
       if (cardArray[i].name === "white")
@@ -71,25 +72,22 @@ document.addEventListener('DOMContentLoaded', () => {
       card.addEventListener('click', flipCard)
       grid.appendChild(card)
     }
-    setTimeout(hide, 4000)
+    setTimeout(hide, 3000)
   }
   //check for matches
   function checkForMatch() {
     if (String(cardsChosen)==String(cardsCompair)) {
-      const cardss = document.querySelectorAll('img')
+      let cardss = document.querySelectorAll('img')
       for (let i = 0; i < cardArray.length; i++) {
-      if (cardArray[i].name === "tree")
-        {cardss[i].setAttribute('src', 'images/tree.png')
+        if (cardArray[i].name === "tree") {
+            cardss[i].setAttribute('src', 'images/tree.png');
+            alert('You found a match')
+            break
         }
+
+      }
     }
-     alert('You found a match')
-    }
-    else {
-      //cards[optionOneId].setAttribute('src', 'images/blank.png')
-      //cards[optionTwoId].setAttribute('src', 'images/blank.png')
-      alert('Sorry, try again')
-    }
-    resultDisplay.textContent = 'Congratulations! You found them all!'
+    resultDisplay.textContent = 'Sorry, try next time'
     cardsChosen = []
     //resultDisplay.textContent = cardsWon.length
   }
@@ -108,7 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //create hide function
   function hide() {
-    const cards = document.querySelectorAll('img')
+    let cards = document.querySelectorAll('img')
     for (let i = 0; i < cardArray.length; i++) {
      if (cardArray[i].name === "tree")
         {cards[i].setAttribute('src', 'images/white.png')
@@ -117,4 +115,263 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   createBoard()
-})
+//})
+};
+/////////
+
+
+async function getData(cityId){
+    try {
+        var response = await fetch('http://127.0.0.1:5001/'+cityId);
+        var data = await response.json();
+        return data
+    } catch (error) {
+        console.log(error.message);
+    } finally {
+    }
+
+};
+
+
+
+
+function weatherData(a) {
+  //console.log(a); // "Some User token"
+  let temp = JSON.parse(JSON.stringify(a));
+  let city = temp['name'];
+  //var description = temp['weather'][0]['description'];
+  //var icon = "http://openweathermap.org/img/wn/" + temp['weather'][0]['icon'] + ".png";
+  var temper = Math.round(temp['main']['temp'] - 273.15);  // in F
+  //var humid = temp['main']['humidity'];
+  //var windSpeed = temp['wind']['speed'];
+  //console.log(temp['weather'][0]['icon']);
+  //document.getElementById(b).innerHTML += "<img src="+icon+" alt=''></img> <br>";
+  //document.getElementById(b).innerHTML += city+"<br>";
+  //document.getElementById(b).innerHTML += description+"<br>";
+  //document.querySelector('.temps').innerHTML += "Hello World!" = "T: " + temper + "°C";
+  document.getElementById("nextAirport Name").innerHTML = city;
+  document.getElementById("nextAirport temp").innerHTML = temper;
+  //document.getElementById(b).innerHTML += "H: "+humid+ "%";
+  //alert(temper)
+}
+
+
+
+///////////////
+
+/*
+let co2Consumed = 0
+let disConsumed = 0
+function maingame() {
+  let points = Math.floor(Math.random() * 6) + 3);
+    for (let i = 0; i < points; i++) {
+      alert('Choose another airport')
+      const card = document.createElement('img')
+      card.setAttribute('src', cardArray[i].img)
+      card.setAttribute('id', i)
+      if (cardArray[i].name === "white")
+        {cardsCompair.push(i)
+        }
+      card.addEventListener('click', flipCard)
+      grid.appendChild(card)
+    }
+    setTimeout(hide, 4000)
+  }
+*/
+
+async function getAirportPosition(icao) {
+    try {
+        const response = await fetch('http://127.0.0.1:5001/airport/' + icao);    // starting data download, fetch returns a promise which contains an object of type 'response'
+        const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function// create position array for leaflet library
+        console.log(jsonData.ICAO, jsonData.Name, jsonData.Lat, jsonData.Long);     // log the result to the console
+
+    } catch (error) {
+        console.log(error.message);
+    } finally {                                         // finally = this is executed anyway, whether the execution was successful or not
+        return jsonData.Lat, jsonData.Long;
+    }
+};
+
+async function getAirportDistance(icao_start, icao_end) {
+    let jsonData;
+    try {
+        const response = await fetch('http://127.0.0.1:5001/airportdistance/' + icao_start + '/' + icao_end);    // starting data download, fetch returns a promise which contains an object of type 'response'
+        jsonData = await response.json();          // http://127.0.0.1:5001/airportdistance/EFHA/EFKT  g the data retrieved from the response object using the json() function// log the result to the console
+        let x = jsonData.dist;
+    } catch (error) {
+        console.log(error.message);
+    } finally {                                         // finally = this is executed anyway, whether the execution was successful or not
+        return document.getElementById("distance").innerHTML = jsonData.dist;
+    }
+    console.log(x);
+};
+function checkDistance() {
+    if (list1.length == 2) {
+        getAirportDistance(list1[0], list1[1]);
+        list1 = []
+    }
+    else {
+       alert('Choose another airport')
+    }
+
+}
+
+
+let icao = "";
+list1 = [];
+/*
+function myFunction_Helsinki {
+    var city = getData(658225)
+    city.then((value) => weatherData(value));
+    icao = 'EFHK';
+    list1.push(icao);
+    checkDistance()
+};
+*/
+function myFunction_Mariehamn() {
+    var city = getData(3041732)
+    city.then((value) => weatherData(value));
+    icao = 'EFMA';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Turku() {
+    var city = getData(633679)
+    city.then((value) => weatherData(value));
+    icao = 'EFTU';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Utti() {
+    var city = getData(633259)
+    city.then((value) => weatherData(value));
+    icao = 'EFUT';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Pori() {
+    var city = getData(640999)
+    city.then((value) => weatherData(value));
+    icao = 'EFPO';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Tampere() {
+    city = getData(634963)
+    city.then((value) => weatherData(value));
+    icao = 'EFTP';
+    list1.push(icao);
+    checkDistance()
+};
+
+function myFunction_Savonlvarinna() {
+    var city = getData(637292)
+    city.then((value) => weatherData(value));
+    icao = 'EFSA';
+    list1.push(icao);
+    checkDistance()
+};
+
+function myFunction_Halli() {
+    var city = getData(650182)
+    city.then((value) => weatherData(value));
+    icao = 'EFHA';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Jyväskylä() {
+    var city = getData(655194)
+    city.then((value) => weatherData(value));
+    icao = 'EFJY';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Vaasa() {
+    var city = getData(632978)
+    city.then((value) => weatherData(value));
+    icao = 'EFVA';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Kuopio() {
+    var city = getData(650224)
+    city.then((value) => weatherData(value));
+    icao = 'EFKU';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Joensuu() {
+    var city = getData(655808)
+    city.then((value) => weatherData(value));
+    icao = 'EFJO';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Kokkola() {
+    var city = getData(651943)
+        city.then((value) => weatherData(value));
+        icao = 'EFKK';
+        list1.push(icao);
+        checkDistance()
+};
+
+function myFunction_Kajaani() {
+    var city = getData(654899)
+    city.then((value) => weatherData(value));
+    icao = 'EFKI';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Oulu() {
+    var city = getData(643492)
+    city.then((value) => weatherData(value));
+    icao = 'EFOU';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Kemi() {
+        city = getData(653281)
+        city.then((value) => weatherData(value));
+        icao = 'EFKE';
+        list1.push(icao);
+        checkDistance()
+};
+
+function myFunction_Kuusamvaro() {
+    var city = getData(649924)
+    city.then((value) => weatherData(value));
+    icao = 'EFKS';
+    list1.push(icao);
+    checkDistance()
+};
+function myFunction_Rovaniemi() {
+    var city = getData(638936)
+    city.then((value) => weatherData(value));
+    icao = 'EFRO';
+    list1.push(icao);
+    checkDistance()
+};
+
+function myFunction_Enontekiö() {
+    var city = getData(660229)
+    city.then((value) => weatherData(value));
+    icao = 'EFET';
+    list1.push(icao);
+    checkDistance()
+};
+
+function myFunction_Kit() {
+  var city = getData(652590);
+  city.then((value) => weatherData(value));
+  icao = 'EFKT';
+  list1.push(icao);
+  checkDistance()
+};
+function myFunction_Iva() {
+  var city = getData(656220);
+  city.then((value) => weatherData(value));
+  icao = 'EFHA';
+  list1.push(icao);
+  checkDistance();
+  runMinigame()
+};
--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+from flask import Flask, request    # this is needed for the HTTP server
+from flask_cors import CORS         # this is needed if you want JavaScript access
+import mysql.connector              # this is needed if you need SQL database
+from geopy import distance
+import requests
+import json
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+db_connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='root',
+    password='Nu151200',
+    autocommit=True
+)
+
+def fetch_airport_names_by_icao_code(icao):
+    sql  = "SELECT latitude_deg,longitude_deg FROM airport"
+    sql += " WHERE ident='" + icao + "'"
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(sql)
+    query_result = db_cursor.fetchall()
+    for row in query_result:        # get only the first match
+        return row[1], row[2]
+
+    return "", ""
+@app.route('/airportdistance/<a>/<b>')
+def calculate_distance_between_airports(a,b):
+#    args = request.args
+#   a = fetch_airport_names_by_icao_code(args.get("start"))
+#    b = fetch_airport_names_by_icao_code(args.get("end"))
+
+    mycursor = db_connection.cursor()
+    mycursor.execute(f"select latitude_deg, longitude_deg from airport where ident = '{a}' or ident = '{b}';")
+    listDeg = []
+    for x in mycursor:
+        listDeg.append(x)
+    d = distance.distance(listDeg[0], listDeg[1]).km
+    return {"dist": d
+    }
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@app.route('/<cityId>')
+def weather(cityId):
+    complete_api_link = f"https://api.openweathermap.org/data/2.5/weather?id={cityId}&appid=daf7a8ec18540e7c2412006de1036a97"
+    api_link = requests.get(complete_api_link).json()
+    return json.dumps(api_link)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(use_reloader=True, host='127.0.0.1', port=5001)
