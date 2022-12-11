a773ac981f3a27a08de5348f93111b9607d581db
AnnaHyttinen <anna.maija.hyttinen@gmail.com>
2022-12-11 11:07:13 +0200

    Distances.

--- main.py
+++ main.py
@@ -42,3 +42,48 @@ print(co2_consumed)
 sql = "UPDATE game SET co2_consumed ='" + co2_consumed + "' WHERE screen_name ='" + screen_name + "';"
 cursor = connection.cursor()
 cursor.execute(sql)
+
+
+
+
+# Calculating the distance using the geopy library:
+
+import geopy
+from geopy import distance
+global distance
+def Distance(icao1, icao2):
+    sql = "SELECT latitude_deg FROM airport WHERE ident='" + icao1 + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    lat1 = cursor.fetchone()
+    sql = "SELECT longitude_deg FROM airport WHERE ident='" + icao1 + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    long1 = cursor.fetchone()
+    sql = "SELECT latitude_deg FROM airport WHERE ident='" + icao2 + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    lat2 = cursor.fetchone()
+    sql = "SELECT longitude_deg FROM airport WHERE ident='" + icao2 + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    long2 = cursor.fetchone()
+
+    lat1 = str(lat1)
+    long1 = str(long1)
+    lat2 = str(lat2)
+    long2 = str(long2)
+
+    lat1 = lat1[1:-2]
+    long1 = long1[1:-2]
+    lat2 = lat2[1:-2]
+    long2 = long2[1:-2]
+
+    lat1 = float(lat1)
+    long1 = float(long1)
+    lat2 = float(lat2)
+    long2 = float(long2)
+
+    distance = distance.distance((lat1, long1), (lat2, long2)).km
+    distance = int(distance)
+    print(f"Distance between places is {distance} km.")
